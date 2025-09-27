ce6b1bb87c2a1835651305a37c86f60f279a3fca
weboutright <weboutright@gmail.com>
2025-09-27 18:11:23 +1000

    h

--- config.js
+++
@@ -1,6 +0,0 @@
-// Simple Booking System Configuration
-// Your Google Apps Script Web App URL
-const API_URL = 'https://script.google.com/macros/s/AKfycbyHUClV1dCLZkH2DOnpVUax2lC38fLLOVkx3SUHUsyvtYcaBriraioJzfcU1S38QpUtEw/exec';
-
-// Configuration is now ready to use!
-// The API_URL variable above will be used by all your HTML pages.
